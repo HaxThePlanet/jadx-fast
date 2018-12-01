feilongluo/jadx

fix: implement new type inference approach

jadx-core/src/main/java/jadx/core/dex/visitors/debuginfo/DebugInfoParser.java
@@ -1,21 +1,21 @@
-package jadx.core.dex.nodes.parser;
+package jadx.core.dex.visitors.debuginfo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.android.dex.Dex.Section;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import jadx.core.deobf.NameMapper;
 import jadx.core.dex.attributes.nodes.SourceFileAttr;
-import jadx.core.dex.instructions.args.InsnArg;
 import jadx.core.dex.instructions.args.RegisterArg;
-import jadx.core.dex.instructions.args.SSAVar;
 import jadx.core.dex.nodes.DexNode;
 import jadx.core.dex.nodes.InsnNode;
 import jadx.core.dex.nodes.MethodNode;
 import jadx.core.utils.exceptions.DecodeException;
 
 public class DebugInfoParser {
-
+	private static final Logger LOG = LoggerFactory.getLogger(DebugInfoParser.class);
 	private static final int DBG_END_SEQUENCE = 0x00;
 	private static final int DBG_ADVANCE_PC = 0x01;
 	private static final int DBG_ADVANCE_LINE = 0x02;
@@ -39,8 +39,9 @@ public class DebugInfoParser {
 	private final DexNode dex;
 
 	private final LocalVar[] locals;
-	private final InsnArg[] activeRegisters;
 	private final InsnNode[] insnByOffset;
+
+	private List<LocalVar> resultList;
 
 	public DebugInfoParser(MethodNode mth, int debugOffset, InsnNode[] insnByOffset) {
 		this.mth = mth;
@@ -49,37 +50,36 @@ public class DebugInfoParser {
 
 		int regsCount = mth.getRegsCount();
 		this.locals = new LocalVar[regsCount];
-		this.activeRegisters = new InsnArg[regsCount];
 		this.insnByOffset = insnByOffset;
 	}
 
-	public void process() throws DecodeException {
+	public List<LocalVar> process() throws DecodeException {
+		boolean varsInfoFound = false;
+		resultList = new ArrayList<>();
+
 		int addr = 0;
 		int line = section.readUleb128();
 
 		int paramsCount = section.readUleb128();
 		List<RegisterArg> mthArgs = mth.getArguments(false);
+
 		for (int i = 0; i < paramsCount; i++) {
-			int id = section.readUleb128() - 1;
-			if (id != DexNode.NO_INDEX) {
-				String name = dex.getString(id);
-				if (i < mthArgs.size()) {
-					mthArgs.get(i).setName(name);
+			int nameId = section.readUleb128() - 1;
+			if (nameId != DexNode.NO_INDEX) {
+				String name = dex.getString(nameId);
+				if (i < mthArgs.size() && name != null) {
+					RegisterArg arg = mthArgs.get(i);
+					int regNum = arg.getRegNum();
+					LocalVar lVar = new LocalVar(regNum, name, arg.getType());
+					startVar(lVar, -1);
+					varsInfoFound = true;
 				}
 			}
-		}
-
-		for (RegisterArg arg : mthArgs) {
-			int rn = arg.getRegNum();
-			locals[rn] = new LocalVar(arg);
-			activeRegisters[rn] = arg;
 		}
 
 		// process '0' instruction
 		addrChange(-1, 1, line);
 		setLine(addr, line);
-
-		boolean varsInfoFound = false;
 
 		int c = section.readByte() & 0xFF;
 		while (c != DBG_END_SEQUENCE) {
@@ -100,7 +100,7 @@ public class DebugInfoParser {
 					int nameId = section.readUleb128() - 1;
 					int type = section.readUleb128() - 1;
 					LocalVar var = new LocalVar(dex, regNum, nameId, type, DexNode.NO_INDEX);
-					startVar(var, addr, line);
+					startVar(var, addr);
 					varsInfoFound = true;
 					break;
 				}
@@ -110,19 +110,13 @@ public class DebugInfoParser {
 					int type = section.readUleb128() - 1;
 					int sign = section.readUleb128() - 1;
 					LocalVar var = new LocalVar(dex, regNum, nameId, type, sign);
-					startVar(var, addr, line);
+					startVar(var, addr);
 					varsInfoFound = true;
 					break;
 				}
 				case DBG_RESTART_LOCAL: {
 					int regNum = section.readUleb128();
-					LocalVar var = locals[regNum];
-					if (var != null) {
-						if (var.end(addr, line)) {
-							setVar(var);
-						}
-						var.start(addr, line);
-					}
+					restartVar(regNum, addr);
 					varsInfoFound = true;
 					break;
 				}
@@ -130,8 +124,7 @@ public class DebugInfoParser {
 					int regNum = section.readUleb128();
 					LocalVar var = locals[regNum];
 					if (var != null) {
-						var.end(addr, line);
-						setVar(var);
+						endVar(var, addr);
 					}
 					varsInfoFound = true;
 					break;
@@ -153,10 +146,10 @@ public class DebugInfoParser {
 
 				default: {
 					if (c >= DBG_FIRST_SPECIAL) {
-						int adjustedOpcode = c - DBG_FIRST_SPECIAL;
-						int addrInc = adjustedOpcode / DBG_LINE_RANGE;
+						int adjustedOpCode = c - DBG_FIRST_SPECIAL;
+						int addrInc = adjustedOpCode / DBG_LINE_RANGE;
 						addr = addrChange(addr, addrInc, line);
-						line += DBG_LINE_BASE + adjustedOpcode % DBG_LINE_RANGE;
+						line += DBG_LINE_BASE + adjustedOpCode % DBG_LINE_RANGE;
 						setLine(addr, line);
 					} else {
 						throw new DecodeException("Unknown debug insn code: " + c);
@@ -170,33 +163,19 @@ public class DebugInfoParser {
 		if (varsInfoFound) {
 			for (LocalVar var : locals) {
 				if (var != null && !var.isEnd()) {
-					var.end(mth.getCodeSize() - 1, line);
-					setVar(var);
+					endVar(var, mth.getCodeSize() - 1);
 				}
 			}
 		}
 		setSourceLines(addr, insnByOffset.length, line);
+
+		return resultList;
 	}
 
 	private int addrChange(int addr, int addrInc, int line) {
 		int newAddr = addr + addrInc;
 		int maxAddr = insnByOffset.length - 1;
 		newAddr = Math.min(newAddr, maxAddr);
-		for (int i = addr + 1; i <= newAddr; i++) {
-			InsnNode insn = insnByOffset[i];
-			if (insn == null) {
-				continue;
-			}
-			for (InsnArg arg : insn.getArguments()) {
-				if (arg.isRegister()) {
-					activeRegisters[((RegisterArg) arg).getRegNum()] = arg;
-				}
-			}
-			RegisterArg res = insn.getResult();
-			if (res != null) {
-				activeRegisters[res.getRegNum()] = res;
-			}
-		}
 		setSourceLines(addr, newAddr, line);
 		return newAddr;
 	}
@@ -214,81 +193,30 @@ public class DebugInfoParser {
 		}
 	}
 
-	private void startVar(LocalVar var, int addr, int line) {
-		int regNum = var.getRegNum();
+	private void restartVar(int regNum, int addr) {
 		LocalVar prev = locals[regNum];
-		if (prev != null && !prev.isEnd()) {
-			prev.end(addr, line);
-			setVar(prev);
-		}
-		InsnArg activeReg = activeRegisters[var.getRegNum()];
-		if (activeReg instanceof RegisterArg) {
-			SSAVar ssaVar = ((RegisterArg) activeReg).getSVar();
-			if (ssaVar != null && ssaVar.getStartAddr() != -1) {
-				InsnNode parentInsn = ssaVar.getAssign().getParentInsn();
-				if (parentInsn != null && parentInsn.getOffset() >= 0) {
-					addr = parentInsn.getOffset();
-				}
-			}
-		}
-		var.start(addr, line);
-		locals[regNum] = var;
-	}
-
-	private void setVar(LocalVar var) {
-		int start = var.getStartAddr();
-		int end = var.getEndAddr();
-
-		for (int i = start; i <= end; i++) {
-			InsnNode insn = insnByOffset[i];
-			if (insn != null) {
-				fillLocals(insn, var);
-			}
-		}
-		merge(activeRegisters[var.getRegNum()], var);
-	}
-
-	private static void fillLocals(InsnNode insn, LocalVar var) {
-		merge(insn.getResult(), var);
-		for (InsnArg arg : insn.getArguments()) {
-			merge(arg, var);
-		}
-	}
-
-	private static void merge(InsnArg arg, LocalVar var) {
-		if (arg == null || !arg.isRegister()) {
-			return;
-		}
-		RegisterArg reg = (RegisterArg) arg;
-		if (var.getRegNum() != reg.getRegNum()) {
-			return;
-		}
-		boolean mergeRequired = false;
-
-		SSAVar ssaVar = reg.getSVar();
-		if (ssaVar != null) {
-			int ssaEnd = ssaVar.getEndAddr();
-			int ssaStart = ssaVar.getStartAddr();
-			int localStart = var.getStartAddr();
-			int localEnd = var.getEndAddr();
-
-			boolean isIntersected = !(localEnd < ssaStart || ssaEnd < localStart);
-			if (isIntersected && ssaEnd <= localEnd) {
-				mergeRequired = true;
-			}
+		if (prev != null) {
+			endVar(prev, addr);
+			LocalVar newVar = new LocalVar(regNum, prev.getName(), prev.getType());
+			startVar(newVar, addr);
 		} else {
-			mergeRequired = true;
-		}
-
-		if (mergeRequired) {
-			applyDebugInfo(reg, var);
+			mth.addComment("Debug info: failed to restart local var, previous not found, register: " + regNum);
 		}
 	}
 
-	private static void applyDebugInfo(RegisterArg reg, LocalVar var) {
-		String varName = var.getName();
-		if (NameMapper.isValidIdentifier(varName)) {
-			reg.mergeDebugInfo(var.getType(), varName);
+	private void startVar(LocalVar newVar, int addr) {
+		int regNum = newVar.getRegNum();
+		LocalVar prev = locals[regNum];
+		if (prev != null) {
+			endVar(prev, addr);
+		}
+		newVar.start(addr);
+		locals[regNum] = newVar;
+	}
+
+	private void endVar(LocalVar var, int addr) {
+		if (var.end(addr)) {
+			resultList.add(var);
 		}
 	}
 }

HaxThePlanet/jadx-fast

jadx-core/src/main/java/jadx/core/dex/visitors/debuginfo/DebugInfoParser.java
@@ -1,21 +1,21 @@
-package jadx.core.dex.nodes.parser;
+package jadx.core.dex.visitors.debuginfo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.android.dex.Dex.Section;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import jadx.core.deobf.NameMapper;
 import jadx.core.dex.attributes.nodes.SourceFileAttr;
-import jadx.core.dex.instructions.args.InsnArg;
 import jadx.core.dex.instructions.args.RegisterArg;
-import jadx.core.dex.instructions.args.SSAVar;
 import jadx.core.dex.nodes.DexNode;
 import jadx.core.dex.nodes.InsnNode;
 import jadx.core.dex.nodes.MethodNode;
 import jadx.core.utils.exceptions.DecodeException;
 
 public class DebugInfoParser {
-
+	private static final Logger LOG = LoggerFactory.getLogger(DebugInfoParser.class);
 	private static final int DBG_END_SEQUENCE = 0x00;
 	private static final int DBG_ADVANCE_PC = 0x01;
 	private static final int DBG_ADVANCE_LINE = 0x02;
@@ -39,8 +39,9 @@ public class DebugInfoParser {
 	private final DexNode dex;
 
 	private final LocalVar[] locals;
-	private final InsnArg[] activeRegisters;
 	private final InsnNode[] insnByOffset;
+
+	private List<LocalVar> resultList;
 
 	public DebugInfoParser(MethodNode mth, int debugOffset, InsnNode[] insnByOffset) {
 		this.mth = mth;
@@ -49,37 +50,36 @@ public class DebugInfoParser {
 
 		int regsCount = mth.getRegsCount();
 		this.locals = new LocalVar[regsCount];
-		this.activeRegisters = new InsnArg[regsCount];
 		this.insnByOffset = insnByOffset;
 	}
 
-	public void process() throws DecodeException {
+	public List<LocalVar> process() throws DecodeException {
+		boolean varsInfoFound = false;
+		resultList = new ArrayList<>();
+
 		int addr = 0;
 		int line = section.readUleb128();
 
 		int paramsCount = section.readUleb128();
 		List<RegisterArg> mthArgs = mth.getArguments(false);
+
 		for (int i = 0; i < paramsCount; i++) {
-			int id = section.readUleb128() - 1;
-			if (id != DexNode.NO_INDEX) {
-				String name = dex.getString(id);
-				if (i < mthArgs.size()) {
-					mthArgs.get(i).setName(name);
+			int nameId = section.readUleb128() - 1;
+			if (nameId != DexNode.NO_INDEX) {
+				String name = dex.getString(nameId);
+				if (i < mthArgs.size() && name != null) {
+					RegisterArg arg = mthArgs.get(i);
+					int regNum = arg.getRegNum();
+					LocalVar lVar = new LocalVar(regNum, name, arg.getInitType());
+					startVar(lVar, -1);
+					varsInfoFound = true;
 				}
 			}
-		}
-
-		for (RegisterArg arg : mthArgs) {
-			int rn = arg.getRegNum();
-			locals[rn] = new LocalVar(arg);
-			activeRegisters[rn] = arg;
 		}
 
 		// process '0' instruction
 		addrChange(-1, 1, line);
 		setLine(addr, line);
-
-		boolean varsInfoFound = false;
 
 		int c = section.readByte() & 0xFF;
 		while (c != DBG_END_SEQUENCE) {
@@ -100,7 +100,7 @@ public class DebugInfoParser {
 					int nameId = section.readUleb128() - 1;
 					int type = section.readUleb128() - 1;
 					LocalVar var = new LocalVar(dex, regNum, nameId, type, DexNode.NO_INDEX);
-					startVar(var, addr, line);
+					startVar(var, addr);
 					varsInfoFound = true;
 					break;
 				}
@@ -110,19 +110,13 @@ public class DebugInfoParser {
 					int type = section.readUleb128() - 1;
 					int sign = section.readUleb128() - 1;
 					LocalVar var = new LocalVar(dex, regNum, nameId, type, sign);
-					startVar(var, addr, line);
+					startVar(var, addr);
 					varsInfoFound = true;
 					break;
 				}
 				case DBG_RESTART_LOCAL: {
 					int regNum = section.readUleb128();
-					LocalVar var = locals[regNum];
-					if (var != null) {
-						if (var.end(addr, line)) {
-							setVar(var);
-						}
-						var.start(addr, line);
-					}
+					restartVar(regNum, addr);
 					varsInfoFound = true;
 					break;
 				}
@@ -130,8 +124,7 @@ public class DebugInfoParser {
 					int regNum = section.readUleb128();
 					LocalVar var = locals[regNum];
 					if (var != null) {
-						var.end(addr, line);
-						setVar(var);
+						endVar(var, addr);
 					}
 					varsInfoFound = true;
 					break;
@@ -153,10 +146,10 @@ public class DebugInfoParser {
 
 				default: {
 					if (c >= DBG_FIRST_SPECIAL) {
-						int adjustedOpcode = c - DBG_FIRST_SPECIAL;
-						int addrInc = adjustedOpcode / DBG_LINE_RANGE;
+						int adjustedOpCode = c - DBG_FIRST_SPECIAL;
+						int addrInc = adjustedOpCode / DBG_LINE_RANGE;
 						addr = addrChange(addr, addrInc, line);
-						line += DBG_LINE_BASE + adjustedOpcode % DBG_LINE_RANGE;
+						line += DBG_LINE_BASE + adjustedOpCode % DBG_LINE_RANGE;
 						setLine(addr, line);
 					} else {
 						throw new DecodeException("Unknown debug insn code: " + c);
@@ -170,33 +163,19 @@ public class DebugInfoParser {
 		if (varsInfoFound) {
 			for (LocalVar var : locals) {
 				if (var != null && !var.isEnd()) {
-					var.end(mth.getCodeSize() - 1, line);
-					setVar(var);
+					endVar(var, mth.getCodeSize() - 1);
 				}
 			}
 		}
 		setSourceLines(addr, insnByOffset.length, line);
+
+		return resultList;
 	}
 
 	private int addrChange(int addr, int addrInc, int line) {
 		int newAddr = addr + addrInc;
 		int maxAddr = insnByOffset.length - 1;
 		newAddr = Math.min(newAddr, maxAddr);
-		for (int i = addr + 1; i <= newAddr; i++) {
-			InsnNode insn = insnByOffset[i];
-			if (insn == null) {
-				continue;
-			}
-			for (InsnArg arg : insn.getArguments()) {
-				if (arg.isRegister()) {
-					activeRegisters[((RegisterArg) arg).getRegNum()] = arg;
-				}
-			}
-			RegisterArg res = insn.getResult();
-			if (res != null) {
-				activeRegisters[res.getRegNum()] = res;
-			}
-		}
 		setSourceLines(addr, newAddr, line);
 		return newAddr;
 	}
@@ -214,81 +193,30 @@ public class DebugInfoParser {
 		}
 	}
 
-	private void startVar(LocalVar var, int addr, int line) {
-		int regNum = var.getRegNum();
+	private void restartVar(int regNum, int addr) {
 		LocalVar prev = locals[regNum];
-		if (prev != null && !prev.isEnd()) {
-			prev.end(addr, line);
-			setVar(prev);
-		}
-		InsnArg activeReg = activeRegisters[var.getRegNum()];
-		if (activeReg instanceof RegisterArg) {
-			SSAVar ssaVar = ((RegisterArg) activeReg).getSVar();
-			if (ssaVar != null && ssaVar.getStartAddr() != -1) {
-				InsnNode parentInsn = ssaVar.getAssign().getParentInsn();
-				if (parentInsn != null && parentInsn.getOffset() >= 0) {
-					addr = parentInsn.getOffset();
-				}
-			}
-		}
-		var.start(addr, line);
-		locals[regNum] = var;
-	}
-
-	private void setVar(LocalVar var) {
-		int start = var.getStartAddr();
-		int end = var.getEndAddr();
-
-		for (int i = start; i <= end; i++) {
-			InsnNode insn = insnByOffset[i];
-			if (insn != null) {
-				fillLocals(insn, var);
-			}
-		}
-		merge(activeRegisters[var.getRegNum()], var);
-	}
-
-	private static void fillLocals(InsnNode insn, LocalVar var) {
-		merge(insn.getResult(), var);
-		for (InsnArg arg : insn.getArguments()) {
-			merge(arg, var);
-		}
-	}
-
-	private static void merge(InsnArg arg, LocalVar var) {
-		if (arg == null || !arg.isRegister()) {
-			return;
-		}
-		RegisterArg reg = (RegisterArg) arg;
-		if (var.getRegNum() != reg.getRegNum()) {
-			return;
-		}
-		boolean mergeRequired = false;
-
-		SSAVar ssaVar = reg.getSVar();
-		if (ssaVar != null) {
-			int ssaEnd = ssaVar.getEndAddr();
-			int ssaStart = ssaVar.getStartAddr();
-			int localStart = var.getStartAddr();
-			int localEnd = var.getEndAddr();
-
-			boolean isIntersected = !(localEnd < ssaStart || ssaEnd < localStart);
-			if (isIntersected && ssaEnd <= localEnd) {
-				mergeRequired = true;
-			}
+		if (prev != null) {
+			endVar(prev, addr);
+			LocalVar newVar = new LocalVar(regNum, prev.getName(), prev.getType());
+			startVar(newVar, addr);
 		} else {
-			mergeRequired = true;
-		}
-
-		if (mergeRequired) {
-			applyDebugInfo(reg, var);
+			mth.addComment("Debug info: failed to restart local var, previous not found, register: " + regNum);
 		}
 	}
 
-	private static void applyDebugInfo(RegisterArg reg, LocalVar var) {
-		String varName = var.getName();
-		if (NameMapper.isValidIdentifier(varName)) {
-			reg.mergeDebugInfo(var.getType(), varName);
+	private void startVar(LocalVar newVar, int addr) {
+		int regNum = newVar.getRegNum();
+		LocalVar prev = locals[regNum];
+		if (prev != null) {
+			endVar(prev, addr);
+		}
+		newVar.start(addr);
+		locals[regNum] = newVar;
+	}
+
+	private void endVar(LocalVar var, int addr) {
+		if (var.end(addr)) {
+			resultList.add(var);
 		}
 	}
 }