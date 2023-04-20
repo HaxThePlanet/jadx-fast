AliFaizaan/jadx

feat(gui): add auto complete for jadx scripts

jadx-gui/src/main/java/jadx/gui/plugins/script/JadxScriptCompleteProvider.java
@@ -1,0 +1,154 @@
+package jadx.gui.plugins.script;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import javax.swing.Icon;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.JTextComponent;
+
+import org.fife.ui.autocomplete.Completion;
+import org.fife.ui.autocomplete.CompletionProviderBase;
+import org.fife.ui.autocomplete.ParameterizedCompletion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import kotlin.script.experimental.api.ScriptDiagnostic;
+import kotlin.script.experimental.api.SourceCode;
+import kotlin.script.experimental.api.SourceCodeCompletionVariant;
+
+import jadx.core.utils.ListUtils;
+import jadx.core.utils.exceptions.JadxRuntimeException;
+import jadx.gui.ui.codearea.AbstractCodeArea;
+import jadx.gui.utils.UiUtils;
+import jadx.plugins.script.ide.JadxScriptAutoComplete;
+import jadx.plugins.script.ide.ScriptCompletionResult;
+
+import static jadx.plugins.script.ide.CompleteKt.AUTO_COMPLETE_INSERT_STR;
+
+public class JadxScriptCompleteProvider extends CompletionProviderBase {
+	private static final Logger LOG = LoggerFactory.getLogger(JadxScriptCompleteProvider.class);
+
+	private static final Map<String, Icon> ICONS_MAP = buildIconsMap();
+
+	private static Map<String, Icon> buildIconsMap() {
+		Map<String, Icon> map = new HashMap<>();
+		map.put("class", UiUtils.ICON_CLASS);
+		map.put("method", UiUtils.ICON_METHOD);
+		map.put("field", UiUtils.ICON_FIELD);
+		map.put("property", UiUtils.ICON_PROPERTY);
+		map.put("parameter", UiUtils.ICON_PARAMETER);
+		map.put("package", UiUtils.ICON_PACKAGE);
+		return map;
+	}
+
+	private final AbstractCodeArea codeArea;
+
+	public JadxScriptCompleteProvider(AbstractCodeArea codeArea) {
+		this.codeArea = codeArea;
+	}
+
+	private List<Completion> getCompletions() {
+		try {
+			String code = codeArea.getText();
+			int caretPos = codeArea.getCaretPosition();
+			JadxScriptAutoComplete scriptComplete = new JadxScriptAutoComplete(codeArea.getNode().getName());
+			ScriptCompletionResult result = scriptComplete.complete(code, caretPos);
+			int replacePos = getReplacePos(caretPos, result);
+			if (!result.getReports().isEmpty()) {
+				LOG.debug("Script completion reports: {}", result.getReports());
+			}
+			return convertCompletions(result.getCompletions(), code, replacePos);
+		} catch (Exception e) {
+			LOG.error("Code completion failed", e);
+			return Collections.emptyList();
+		}
+	}
+
+	private List<Completion> convertCompletions(List<SourceCodeCompletionVariant> completions, String code, int replacePos) {
+		if (completions.isEmpty()) {
+			return Collections.emptyList();
+		}
+		if (LOG.isDebugEnabled()) {
+			String cmplStr = completions.stream().map(SourceCodeCompletionVariant::toString).collect(Collectors.joining("\n"));
+			LOG.debug("Completions:\n{}", cmplStr);
+		}
+		int count = completions.size();
+		List<Completion> list = new ArrayList<>(count);
+		for (int i = 0; i < count; i++) {
+			SourceCodeCompletionVariant c = completions.get(i);
+			if (Objects.equals(c.getIcon(), "keyword")) {
+				// too many, not very useful
+				continue;
+			}
+
+			JadxScriptCompletion cmpl = new JadxScriptCompletion(this, count - i);
+			cmpl.setData(c.getText(), code, replacePos);
+			if (Objects.equals(c.getIcon(), "method") && !Objects.equals(c.getText(), c.getDisplayText())) {
+				// add method args details for methods
+				cmpl.setSummary(c.getDisplayText() + " " + c.getTail());
+			} else {
+				cmpl.setSummary(c.getTail());
+			}
+			cmpl.setToolTip(c.getDisplayText());
+			Icon icon = ICONS_MAP.get(c.getIcon());
+			cmpl.setIcon(icon != null ? icon : UiUtils.FILE_ICON);
+			list.add(cmpl);
+		}
+		return list;
+	}
+
+	private int getReplacePos(int caretPos, ScriptCompletionResult result) throws BadLocationException {
+		int lineRaw = codeArea.getLineOfOffset(caretPos);
+		int lineStart = codeArea.getLineStartOffset(lineRaw);
+		int line = lineRaw + 1;
+		int col = caretPos - lineStart + 1;
+
+		List<ScriptDiagnostic> reports = result.getReports();
+		ScriptDiagnostic cmplReport = ListUtils.filterOnlyOne(reports, r -> {
+			if (r.getSeverity() == ScriptDiagnostic.Severity.ERROR && r.getLocation() != null) {
+				SourceCode.Position start = r.getLocation().getStart();
+				return start.getLine() == line && r.getMessage().endsWith(AUTO_COMPLETE_INSERT_STR);
+			}
+			return false;
+		});
+		if (cmplReport == null) {
+			LOG.warn("Failed to find completion report in: {}", reports);
+			return caretPos;
+		}
+		reports.remove(cmplReport);
+		int reportCol = Objects.requireNonNull(cmplReport.getLocation()).getStart().getCol();
+		return caretPos - (col - reportCol);
+	}
+
+	@Override
+	public String getAlreadyEnteredText(JTextComponent comp) {
+		try {
+			int pos = codeArea.getCaretPosition();
+			return codeArea.getText(0, pos);
+		} catch (Exception e) {
+			throw new JadxRuntimeException("Failed to get text before caret", e);
+		}
+	}
+
+	@Override
+	public List<Completion> getCompletionsAt(JTextComponent comp, Point p) {
+		return getCompletions();
+	}
+
+	@Override
+	protected List<Completion> getCompletionsImpl(JTextComponent comp) {
+		return getCompletions();
+	}
+
+	@Override
+	public List<ParameterizedCompletion> getParameterizedCompletions(JTextComponent tc) {
+		return null;
+	}
+}

jadx-gui/src/main/java/jadx/gui/treemodel/JMethod.java
@@ -22,8 +22,6 @@ import jadx.gui.utils.UiUtils;
 
 public class JMethod extends JNode {
 	private static final long serialVersionUID = 3834526867464663751L;
-
-	private static final ImageIcon ICON_METHOD = UiUtils.openSvgIcon("nodes/method");
 	private static final ImageIcon ICON_METHOD_ABSTRACT = UiUtils.openSvgIcon("nodes/abstractMethod");
 	private static final ImageIcon ICON_METHOD_PRIVATE = UiUtils.openSvgIcon("nodes/privateMethod");
 	private static final ImageIcon ICON_METHOD_PROTECTED = UiUtils.openSvgIcon("nodes/protectedMethod");
@@ -65,7 +63,7 @@ public class JMethod extends JNode {
 	@Override
 	public Icon getIcon() {
 		AccessInfo accessFlags = mth.getAccessFlags();
-		Icon icon = ICON_METHOD;
+		Icon icon = UiUtils.ICON_METHOD;
 		if (accessFlags.isAbstract()) {
 			icon = ICON_METHOD_ABSTRACT;
 		}

HaxThePlanet/jadx-fast

jadx-gui/src/main/java/jadx/gui/plugins/script/JadxScriptCompleteProvider.java
@@ -1,0 +1,154 @@
+package jadx.gui.plugins.script;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import javax.swing.Icon;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.JTextComponent;
+
+import org.fife.ui.autocomplete.Completion;
+import org.fife.ui.autocomplete.CompletionProviderBase;
+import org.fife.ui.autocomplete.ParameterizedCompletion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import kotlin.script.experimental.api.ScriptDiagnostic;
+import kotlin.script.experimental.api.SourceCode;
+import kotlin.script.experimental.api.SourceCodeCompletionVariant;
+
+import jadx.core.utils.ListUtils;
+import jadx.core.utils.exceptions.JadxRuntimeException;
+import jadx.gui.ui.codearea.AbstractCodeArea;
+import jadx.gui.utils.Icons;
+import jadx.plugins.script.ide.JadxScriptAutoComplete;
+import jadx.plugins.script.ide.ScriptCompletionResult;
+
+import static jadx.plugins.script.ide.CompleteKt.AUTO_COMPLETE_INSERT_STR;
+
+public class JadxScriptCompleteProvider extends CompletionProviderBase {
+	private static final Logger LOG = LoggerFactory.getLogger(JadxScriptCompleteProvider.class);
+
+	private static final Map<String, Icon> ICONS_MAP = buildIconsMap();
+
+	private static Map<String, Icon> buildIconsMap() {
+		Map<String, Icon> map = new HashMap<>();
+		map.put("class", Icons.CLASS);
+		map.put("method", Icons.METHOD);
+		map.put("field", Icons.FIELD);
+		map.put("property", Icons.PROPERTY);
+		map.put("parameter", Icons.PARAMETER);
+		map.put("package", Icons.PACKAGE);
+		return map;
+	}
+
+	private final AbstractCodeArea codeArea;
+
+	public JadxScriptCompleteProvider(AbstractCodeArea codeArea) {
+		this.codeArea = codeArea;
+	}
+
+	private List<Completion> getCompletions() {
+		try {
+			String code = codeArea.getText();
+			int caretPos = codeArea.getCaretPosition();
+			JadxScriptAutoComplete scriptComplete = new JadxScriptAutoComplete(codeArea.getNode().getName());
+			ScriptCompletionResult result = scriptComplete.complete(code, caretPos);
+			int replacePos = getReplacePos(caretPos, result);
+			if (!result.getReports().isEmpty()) {
+				LOG.debug("Script completion reports: {}", result.getReports());
+			}
+			return convertCompletions(result.getCompletions(), code, replacePos);
+		} catch (Exception e) {
+			LOG.error("Code completion failed", e);
+			return Collections.emptyList();
+		}
+	}
+
+	private List<Completion> convertCompletions(List<SourceCodeCompletionVariant> completions, String code, int replacePos) {
+		if (completions.isEmpty()) {
+			return Collections.emptyList();
+		}
+		if (LOG.isDebugEnabled()) {
+			String cmplStr = completions.stream().map(SourceCodeCompletionVariant::toString).collect(Collectors.joining("\n"));
+			LOG.debug("Completions:\n{}", cmplStr);
+		}
+		int count = completions.size();
+		List<Completion> list = new ArrayList<>(count);
+		for (int i = 0; i < count; i++) {
+			SourceCodeCompletionVariant c = completions.get(i);
+			if (Objects.equals(c.getIcon(), "keyword")) {
+				// too many, not very useful
+				continue;
+			}
+
+			JadxScriptCompletion cmpl = new JadxScriptCompletion(this, count - i);
+			cmpl.setData(c.getText(), code, replacePos);
+			if (Objects.equals(c.getIcon(), "method") && !Objects.equals(c.getText(), c.getDisplayText())) {
+				// add method args details for methods
+				cmpl.setSummary(c.getDisplayText() + " " + c.getTail());
+			} else {
+				cmpl.setSummary(c.getTail());
+			}
+			cmpl.setToolTip(c.getDisplayText());
+			Icon icon = ICONS_MAP.get(c.getIcon());
+			cmpl.setIcon(icon != null ? icon : Icons.FILE);
+			list.add(cmpl);
+		}
+		return list;
+	}
+
+	private int getReplacePos(int caretPos, ScriptCompletionResult result) throws BadLocationException {
+		int lineRaw = codeArea.getLineOfOffset(caretPos);
+		int lineStart = codeArea.getLineStartOffset(lineRaw);
+		int line = lineRaw + 1;
+		int col = caretPos - lineStart + 1;
+
+		List<ScriptDiagnostic> reports = result.getReports();
+		ScriptDiagnostic cmplReport = ListUtils.filterOnlyOne(reports, r -> {
+			if (r.getSeverity() == ScriptDiagnostic.Severity.ERROR && r.getLocation() != null) {
+				SourceCode.Position start = r.getLocation().getStart();
+				return start.getLine() == line && r.getMessage().endsWith(AUTO_COMPLETE_INSERT_STR);
+			}
+			return false;
+		});
+		if (cmplReport == null) {
+			LOG.warn("Failed to find completion report in: {}", reports);
+			return caretPos;
+		}
+		reports.remove(cmplReport);
+		int reportCol = Objects.requireNonNull(cmplReport.getLocation()).getStart().getCol();
+		return caretPos - (col - reportCol);
+	}
+
+	@Override
+	public String getAlreadyEnteredText(JTextComponent comp) {
+		try {
+			int pos = codeArea.getCaretPosition();
+			return codeArea.getText(0, pos);
+		} catch (Exception e) {
+			throw new JadxRuntimeException("Failed to get text before caret", e);
+		}
+	}
+
+	@Override
+	public List<Completion> getCompletionsAt(JTextComponent comp, Point p) {
+		return getCompletions();
+	}
+
+	@Override
+	protected List<Completion> getCompletionsImpl(JTextComponent comp) {
+		return getCompletions();
+	}
+
+	@Override
+	public List<ParameterizedCompletion> getParameterizedCompletions(JTextComponent tc) {
+		return null;
+	}
+}

jadx-gui/src/main/java/jadx/gui/treemodel/JMethod.java
@@ -23,8 +23,6 @@ import jadx.gui.utils.UiUtils;
 
 public class JMethod extends JNode {
 	private static final long serialVersionUID = 3834526867464663751L;
-
-	private static final ImageIcon ICON_METHOD = UiUtils.openSvgIcon("nodes/method");
 	private static final ImageIcon ICON_METHOD_ABSTRACT = UiUtils.openSvgIcon("nodes/abstractMethod");
 	private static final ImageIcon ICON_METHOD_PRIVATE = UiUtils.openSvgIcon("nodes/privateMethod");
 	private static final ImageIcon ICON_METHOD_PROTECTED = UiUtils.openSvgIcon("nodes/protectedMethod");
@@ -66,7 +64,7 @@ public class JMethod extends JNode {
 	@Override
 	public Icon getIcon() {
 		AccessInfo accessFlags = mth.getAccessFlags();
-		Icon icon = ICON_METHOD;
+		Icon icon = Icons.METHOD;
 		if (accessFlags.isAbstract()) {
 			icon = ICON_METHOD_ABSTRACT;
 		}